faeb61b8ff5c342785ae0b9ab92f7934ca3bbc8a
subbu <subbu@subbaraos-MacBook-Pro.local>
2017-07-09 22:44:09 +0530

    Intigrate Facebook to invent travel

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,23 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+  # General Settings
+  config.app_domain = 'http://localhost:3000'
+
+  # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'noreply@inventtravel.com',
+    :password             => 'itravel123',
+    :authentication       => :plain,
+    :enable_starttls_auto => true,
+    :domain               => 'gmail.com'
+  }
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,6 +251,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  
+  config.omniauth :facebook, "111364312834937", "36d2784e8a5918d6452a68fa91bd7d49"
+  config.omniauth :twitter, "KEY", "SECRET"
+  config.omniauth :linked_in, "KEY", "SECRET"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
